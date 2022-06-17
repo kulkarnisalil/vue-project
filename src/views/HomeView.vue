61afe66f06c0d33f3518dc8152046f3035fe2ff6
salil kulkarni <salilkulkarni8@gmail.com>
2022-06-17 11:09:53 +0530

    chore: vue-project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,21 @@
 import TheWelcome from "@/components/TheWelcome.vue";
 export default {
   components: { TheWelcome },
+  data() {
+    return {
+      message: "Hello World",
+    };
+  },
+  mounted() {
+    alert("I have been mounted");
+  },
 };
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
   </main>
 </template>
+ 

c0f0e47199eb8b19738a70393df6385dcbca2349
salil kulkarni <salilkulkarni8@gmail.com>
2022-06-22 11:06:56 +0530

    chore: vue-project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,29 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 let food = ref(localStorage.getItem("food"));
 let age = ref(localStorage.getItem("age"));
+
+watch(food, (val) => {
+  write("food", val);
+});
 
 function write(key, val) {
   localStorage.setItem(key, val);
 }
+setTimeout(() => {
+  food.value = "changed";
+}, 2000);
 </script>
 
 <template>
   <main>
     <p>
       What is your favorite food?
-      <input type="text" v-model="food" @input="write('food', food)" />
+      <input type="text" v-model="food" />
     </p>
     <p>
       How old are you?
-      <input type="text" v-model="age" @input="write('age', age)" />
+      <input type="text" v-model="age" />
     </p>
   </main>
 </template>

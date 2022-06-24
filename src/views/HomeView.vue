e948b9db4d258ff1c00f73ed89edab186b819572
salil kulkarni <salilkulkarni8@gmail.com>
2022-06-24 15:04:54 +0530

    chore: vue-project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,23 @@
 <script setup>
-import { ref } from "vue";
-
-let textarea = ref(null);
-
 function onTabPress(e) {
-  let t = textarea.value;
+  let textarea = e.target;
   // get caret position/selection
-  let val = t.value,
-    start = t.selectionStart,
-    end = t.selectionEnd;
+  let val = textarea.value,
+    start = textarea.selectionStart,
+    end = textarea.selectionEnd;
 
   // set textarea value to: text before caret + tab + text after caret
-  t.value = val.substring(0, start) + "\t" + val.substring(end);
+  textarea.value = val.substring(0, start) + "\t" + val.substring(end);
 
   // put caret at right position again
-  t.selectionStart = t.selectionEnd = start + 1;
-
-  e.preventDefault();
+  textarea.selectionStart = textarea.selectionEnd = start + 1;
 }
 </script>
 
 <template>
   <main>
     <form>
-      <textarea ref="textarea" @keydown.tab="onTabPress" style="width: 100%; height: 300px">
+      <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px">
 Hi There</textarea>
     </form>
   </main>

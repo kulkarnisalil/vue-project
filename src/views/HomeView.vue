16b0a57767b8e95995c3e724a7b7f8ce4bd1787d
salil kulkarni <salilkulkarni8@gmail.com>
2022-06-24 12:27:08 +0530

    chore: vue-project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,35 @@
 <script setup>
+import { ref } from "vue";
+
+let textarea = ref(null);
+
+console.log(textarea.value);
+
+// textarea.addEventListenere("keydown", (e) => {
+//   // tab was pressed
+//   if (e.keyCode == 9) {
+//     // get caret position/selection
+//     let val =textarea.value,
+//     start = textarea.selectionStart,
+//     end = textarea.selectionEnd;
+
+//     // set textarea value to: text before caret + tab + text after caret
+//     tectarea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+//     // put caret at right position again
+//     textarea.selectionStart = textarea.selectionEnd = start + 1;
+
+//     e.preventDefault();
+//   }
+// })
 </script>
 
 <template>
   <main>
     <form>
-      <textarea style="width: 100%; height: 300px;">Hi There</textarea>
+      <textarea ref="textarea" style="width: 100%; height: 300px">
+Hi There</textarea
+      >
     </form>
   </main>
 </template>
